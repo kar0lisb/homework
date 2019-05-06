2d2b3eefb12e458aee2a86c005d9cbc3643b3029
kar0lisb <49728697+kar0lisb@users.noreply.github.com>
2019-05-06 20:51:14 +0300

    php2

---
+++ darbai/index02.php
@@ -0,0 +1,24 @@
+<?php
+    $miestas = 'Vilnius';
+    $šalis = 'Lietuva';
+    $populiacija = '574221';
+?>
+
+<!DOCTYPE html>
+<html lang="lt">
+    
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php2</title>
+</head>
+
+<body>
+    <div>
+        <p><?php echo $miestas . '- Lietuvos sostinė'; ?> </p>
+        <p><?php echo "$miestas ir $šalis sparčiai auga"; ?> </p>
+    </div>
+    
+</body>
+    
+</html>
