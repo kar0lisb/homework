6e73691565a1294cdc5ad536afdfe9aae11e1c56
kar0lisb <49728697+kar0lisb@users.noreply.github.com>
2019-05-06 20:50:55 +0300

    PHP1

---
+++ darbai/index01.php
@@ -0,0 +1,25 @@
+<?php
+    $miestas = 'Vilnius';
+    $šalis = 'Lietuva';
+    $populiacija = '574221';
+?>
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php1</title>
+</head>
+
+<body>
+    <div>
+        <ul>
+            <li><?php echo $miestas; ?></li>
+            <li><?php echo $šalis; ?></li>
+            <li><?php echo $populiacija; ?></li>
+        </ul>
+    </div>
+</body>
+    
+</html>
