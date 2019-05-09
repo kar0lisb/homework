69f48a5bbff0777e5ce1699fd561db154adc5567
kar0lisb <49728697+kar0lisb@users.noreply.github.com>
2019-05-09 21:54:39 +0300

    php3_1

---
+++ darbai/php3_1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php3_1</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        function plotas($a, $b) {
+            $s = $a * $b;
+            echo $s;
+        }
+        plotas(10, 20);
+        ?>
+    </div>
+    
+</body>
+    
+</html>
